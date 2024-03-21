efc209e60fac18746aa49266dfd619e7e8766f61
OckhamByron <ockham-byron@protonmail.com>
2024-03-21 12:16:45 +0100

    build static

--- core/settings.py
+++ core/settings.py
@@ -154,7 +154,7 @@ LOCALE_PATHS = (
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
